60916f0891fb35e11fa2e09a9166d157df958b8f
nezhniy <nezhniystrastniy@yandex.ru>
2022-12-22 12:24:47 +0500

    8. Date. Отображение даты. [#310017 #373554]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,16 +1,11 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
 
     public Item() {
     }
@@ -22,6 +17,10 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
