57b2be2633e1086c941d8a42458ace1d7ad53e55
nezhniy <nezhniystrastniy@yandex.ru>
2022-12-23 11:45:43 +0500

    3. Массивы и модели. [#174892 #373809]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -6,28 +6,23 @@ public class Library {
         Book fantasy = new Book("Fantasy", 300);
         Book romance = new Book("Romance", 300);
         Book clean = new Book("Clean code", 300);
-
         Book[] arrayOfBooks = new Book[4];
         arrayOfBooks[0] = history;
         arrayOfBooks[1] = fantasy;
         arrayOfBooks[2] = romance;
         arrayOfBooks[3] = clean;
-
         for (int i = 0; i < arrayOfBooks.length; i++) {
             Book book = arrayOfBooks[i];
             System.out.println(book.getName() + " - " + book.getPages() + " pages");
         }
-
         arrayOfBooks[0] = clean;
         arrayOfBooks[3] = history;
-
         for (Book book: arrayOfBooks) {
             System.out.println(book.getName() + " - " + book.getPages() + " pages");
         }
-
         for (int i = 0; i < arrayOfBooks.length; i++) {
             Book book = arrayOfBooks[i];
-            if (book.getName().equals("Clean code")) {
+            if ("Clean code".equals(book.getName())) {
                 System.out.println(book.getName() + " - " + book.getPages() + " pages");
             }
         }
